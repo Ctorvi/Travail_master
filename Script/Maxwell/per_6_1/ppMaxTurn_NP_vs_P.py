f0cba3710adee2de947413c32b1121b76275ccd2
Victor Despland <victordespland@tsf-428-wpa-4-209.epfl.ch>
2026-01-28 16:06:09 +0100

    dvance on oral presentation

--- Script/Maxwell/per_6_1/ppMaxTurn_NP_vs_P.py
+++ Script/Maxwell/per_6_1/ppMaxTurn_NP_vs_P.py
@@ -24,7 +24,11 @@ POINCARE_ITS = 500
 SHEAR = 1.2
 SF = 1.16
 
-fig, (ax, ax2) = plt.subplots(1, 2, figsize=(8, 7), width_ratios=[1,1], constrained_layout=False,sharex=True,sharey=True)
+#fig, (ax, ax2) = plt.subplots(1, 2, figsize=(8, 7), width_ratios=[1,1], constrained_layout=False,sharex=True,sharey=True)
+
+fig, ax = plt.subplots(1, 1, figsize=(6, 6.5))
+
+fig2, ax2 = plt.subplots(1, 1, figsize=(6, 6.5))
 
 separatrix = {"type": "circular-current-loop", "amplitude": -10, "R": 6, "Z": -5.5}
 
@@ -74,7 +78,7 @@ ax.set_aspect('equal')
 ax2.set_xlabel(r"$R[m]$")
 ax2.set_aspect('equal')
 ax.set_ylabel(r"$Z[m]$")
-
+ax2.set_ylabel(r"$Z[m]$")
 
 # perturbedmanifold = Manifold(perturbedmap, xpoint, xpoint)
 # perturbedmanifold.compute(
@@ -111,26 +115,39 @@ opoint.find(t=1, guess=[6., 0.])
 opointcoords = opoint.coords[0]
 opoint.plot(ax=ax2, s=70,marker='o', color="xkcd:darkgreen",zorder=20,label='O-point')
 
+
+
 NP_manifold  = Manifold.load(f"{repository_path}mf_NP.pkl")
-NP_manifold.plot(ax=ax, markersize=0, lw=1.2)
+#NP_manifold.plot(ax=ax, markersize=0, lw=1.2)
 
 P_manifold  = Manifold.load(f"{repository_path}mf.pkl")
 P_manifold.plot_clinics(ax=ax2, s=20,label='Homoclinics')
 P_manifold.plot(ax=ax2, markersize=0, lw=1.2)
 
-ax.text( 0.05, 0.93, 
-   '(a)', transform=ax.transAxes,fontsize=12,fontweight='bold',ha='left',va='top',color='black', zorder=200)
+# ax.text( 0.05, 0.93, 
+#    '(a)', transform=ax.transAxes,fontsize=12,fontweight='bold',ha='left',va='top',color='black', zorder=200)
 
-ax2.text( 0.05, 0.93, 
-   '(b)', transform=ax2.transAxes,fontsize=12,fontweight='bold',ha='left',va='top',color='black', zorder=200)
+# ax2.text( 0.05, 0.93, 
+#    '(b)', transform=ax2.transAxes,fontsize=12,fontweight='bold',ha='left',va='top',color='black', zorder=200)
 
-ax2.legend(loc='lower left', bbox_to_anchor=(-0.02, -0.01), ncol=1, fontsize=7)
+# ax2.legend(loc='lower left', bbox_to_anchor=(-0.02, -0.01), ncol=1, fontsize=7)
 
 #texte=f"Turnstile flux: $\phi = ${perturbedmanifold.turnstile_areas[0]:.3e}"
 
 #ax.text(2, 3, texte, fontsize=12, color='blue')
 
-plt.savefig(f"{repository_path}figure_chaos.png", dpi=720,bbox_inches="tight", pad_inches=0, facecolor=fig.get_facecolor())
+#plt.savefig(f"{repository_path}figure_chaos.png", dpi=720,bbox_inches="tight", pad_inches=0, facecolor=fig.get_facecolor())
 #print(perturbedmanifold.turnstile_areas)
+
+ax.set_xlim(3.2, 9.4)
+ax.set_ylim(-6.2, 2.8)
+ax2.set_xlim(3.2, 9.4)
+ax2.set_ylim(-6.2, 2.8)
+
+
+#fig.savefig(f"{repository_path}oral_max6_1_axi.png", bbox_inches='tight', pad_inches=0.0, dpi=720)
+fig2.savefig(f"{repository_path}oral_max6_1_nonaxi_w_MF.png", bbox_inches='tight', pad_inches=0.0, dpi=720)
+
+
 plt.show()
 
